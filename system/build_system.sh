65276e3a7a814c564fba4b446cdccdcca88e2b85
joobih <1006536507@qq.com>
2017-04-19 17:31:09 +0800

    增加系统安装脚本

---
+++ system/build_system.sh
@@ -0,0 +1,52 @@
+#!/bin bash
+
+sudo apt-get update
+
+#安装vim
+sudo cp vim74_new.tar.gz /opt
+sudo cd /opt
+sudo tar -zxvf vim74_new.tar.gz
+sudo cd vim74
+sudo ./configure
+sudo make
+sudo make install
+
+cp -r vim/ ~/
+cp vimrc ~/.vimrc
+
+#安装python库等
+sudo apt-get install python=2.7.12
+sudo apt-get install python-pip
+sudo pip install -U pip
+sudo pip install tornado
+sudo pip install thrift
+sudo pip install pika
+sudo pip install bs4
+sudo pip install pymongo
+
+#安装pyhs2
+sudo apt-get install libsasl2-dev
+sudo apt-get install libsasl2-modules-gssapi-mit
+sudo pip install pyhs2
+
+#安装scrapy
+sudo apt-get install libxml2-dev libxslt1-dev
+sudo pip install scrapy
+
+
+#安装docker
+sudo apt-get install docker.io
+sudo cp ./daemon.json /etc/docker/daemon.json
+sudo groupadd docker
+sudo gpasswd -a cuijun docker
+sudo service docker restart
+
+#安装rabbitmq
+sudo apt-get install rabbitmq-server
+
+#安装mysql
+sudo apt-get install mysql-server mysql-client
+
+#安装redis
+sudo apt-get install redis-server
+
---
+++ system/vim74/src/auto/pathdef.c
@@ -0,0 +1,10 @@
+/* pathdef.c */
+/* This file is automatically created by Makefile
+ * DO NOT EDIT!  Change Makefile only. */
+#include "vim.h"
+char_u *default_vim_dir = (char_u *)"/usr/local/share/vim";
+char_u *default_vimruntime_dir = (char_u *)"";
+char_u *all_cflags = (char_u *)"gcc -c -I. -Iproto -DHAVE_CONFIG_H   -I/usr/local/include  -O2 -fno-strength-reduce -Wall -U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=1      ";
+char_u *all_lflags = (char_u *)"gcc   -L/usr/local/lib -Wl,--as-needed -o vim        -lm -ltinfo -lnsl   -ldl    -L/usr/lib/python2.7/config-x86_64-linux-gnu -lpython2.7 -lpthread -ldl -lutil -lm -Xlinker -export-dynamic -Wl,-O1 -Wl,-Bsymbolic-functions      ";
+char_u *compiled_user = (char_u *)"cuijun";
+char_u *compiled_sys = (char_u *)"cuijun-pc";
